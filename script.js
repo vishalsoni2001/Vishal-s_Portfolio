0d45f4af45ca06e328ad1e367c8799bcc52c7fda
vishalsoni2001 <112778682+vishalsoni2001@users.noreply.github.com>
2023-06-24 16:33:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "inline-flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "inline-flex";
+  }
+}
+
